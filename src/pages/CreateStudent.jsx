0319fbe1462ba1d52917f764655f3a026373619a
Jonathan Clavijo <jonathanclavijo0907@gmail.com>
2023-07-02 23:45:45 -0500

    feat: Validation form new student

--- src/pages/CreateStudent.jsx
+++ src/pages/CreateStudent.jsx
@@ -11,6 +11,7 @@ const CreateStudent = () => {
     apellido: '',
     edad: '',
     identificacion: '',
+    materias: [],
   });
 
   const dispatch = useDispatch();
@@ -30,6 +31,18 @@ const CreateStudent = () => {
     }));
     navigate('/');
   }
+
+  const handleCheckbox = e => {
+    let newStudent = student;
+    if(e.target.checked) {
+      newStudent.materias.push(Number(e.target.name));
+      setStudent(newStudent);      
+    } else {
+      const filterId = newStudent.materias.filter(item => item !== Number(e.target.name));      
+      newStudent.materias = filterId;
+      setStudent(newStudent);
+    }
+  }
   
   return (
     <div className="bg-white border h-full p-5 shadow-xl rounded-2xl">
@@ -39,6 +52,7 @@ const CreateStudent = () => {
       </div>
       <form onSubmit={handleSubmit} className="mt-8 space-y-10">  
         <input 
+          required
           name="nombre"
           className="bg-transparent w-max border-black border-b pb-1" 
           type="text" 
@@ -46,6 +60,7 @@ const CreateStudent = () => {
           onChange={handleChange}
         />
         <input 
+          required
           name="apellido"
           className="bg-transparent block w-max border-black border-b pb-1" 
           type="text" 
@@ -53,6 +68,7 @@ const CreateStudent = () => {
           onChange={handleChange}
         />
         <input 
+          required
           name="edad"
           className="bg-transparent block w-max border-black border-b pb-1" 
           type="number" 
@@ -60,17 +76,44 @@ const CreateStudent = () => {
           onChange={handleChange}
         />
         <input 
+          required
           name="identificacion"
           className="bg-transparent block w-max border-black border-b pb-1" 
           type="number" 
           placeholder="No. identificación"
           onChange={handleChange}
         />
+        <div className="flex flex-col gap-5">
+            <div>
+              <p className="text-zinc-500 text-xl">Asignar materias al estudiante</p>
+            </div>
+          <div className="flex items-center gap-3 border w-max p-3 shadow-md rounded-lg font-semibold">          
+            <input               
+              type="checkbox" 
+              name="1" 
+              onChange={handleCheckbox}                       
+            />
+            Matemáticas            
+            <input 
+              type="checkbox" 
+              name="2" 
+              onChange={handleCheckbox}                       
+            />
+            Ciencias
+            <input 
+              type="checkbox" 
+              name="3" 
+              onChange={handleCheckbox}                       
+            />
+            Música
+          </div>
+        </div>  
+        
         <input 
           className="bg-blue-500 py-2 px-3 text-white rounded-full shadow-2xl"
           type="submit" 
           value="Crear estudiante"
-        />
+        />        
         <Link
           className="block text-zinc-500"
           to={'/'}
